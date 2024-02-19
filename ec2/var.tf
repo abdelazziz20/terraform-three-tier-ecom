1efff275dd15c6aa58dcd55396005db802d25028
abdelazziz20 <142108226+abdelazziz20@users.noreply.github.com>
2024-02-19 11:31:44 -0600

    Update var.tf

--- ec2/var.tf
+++ ec2/var.tf
@@ -3,6 +3,11 @@ variable "ami" {
     description = "ami id"
     default = "ami-0c20d88b0021158c6"
 }
+variable "key_name" {
+    type        = string
+    description = "The name of the key pair to use for EC2 instances"
+    default     = "test-keypair123"
+}
 
 variable "instance_type" {
     type = string
